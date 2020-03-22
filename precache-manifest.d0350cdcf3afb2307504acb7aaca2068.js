5d7662d944072508362e8f7f042753b439109ddd
chemi mohamed <chemimohamed8@gmail.com>
2020-03-22 01:05:28 +0100

    Updates

---
+++ precache-manifest.d0350cdcf3afb2307504acb7aaca2068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621b60aa3e84bd46d4c8f08cb6b7bb67",
+    "url": "/RJ/index.html"
+  },
+  {
+    "revision": "b1d45d4a5140172a8603",
+    "url": "/RJ/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1014ba326dc0c6b1104d",
+    "url": "/RJ/static/js/2.58bfb1cf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/RJ/static/js/2.58bfb1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d45d4a5140172a8603",
+    "url": "/RJ/static/js/main.deae2578.chunk.js"
+  },
+  {
+    "revision": "c89329c20c9cb356d828",
+    "url": "/RJ/static/js/runtime-main.6c9994d1.js"
+  }
+]);
